8ed6394ea593fe919f523d42200ece6fb275206c
Roos <roosolstafir@hotmail.com>
2018-02-13 17:16:57 -0600

    ok

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ $(document).ready(function(){
       showImage(this);
     });
  });
-var photos = [], size = [];
+var photos = [], size = [], arrayInputFiles = [],
+    validImg = ['image/jpeg','image/jpg','image/png','image/gif'];
 function showImage(image){
    var inputFile = document.getElementById("photos").files;
-   for (var x = 0; x < inputFile.length; x++) {
-      var reader = new FileReader();
+   for (let x = 0; x < inputFile.length; x++) {
+     if ($.inArray(inputFile.item(x).type, validImg) < 0) { // Valida que sea imagén
+       continue;
+     }else if (inputFile.item(x).size > 2e+6) { // Valida que sea menor a 2Mb
+       continue;
+     }
+      let reader = new FileReader();
       reader.onload = function(e){
-         if (!size.includes(e.total) && !photos.includes(e.target.result)) {
+         if (!size.includes(e.total) && !photos.includes(e.target.result) && !arrayInputFiles.includes(inputFile.item(x)) && size.length < 5) {
+            arrayInputFiles.push(inputFile.item(x));
             size.push(e.total);
             photos.push(e.target.result);
          }else {
@@ -18,16 +25,17 @@ function showImage(image){
          for (var i = 0; i < photos.length; i++) {
             for (var r = 0; r < size.length; r++) {
                if (photos.length === 1 && e.total === size[r]) {
-                  $("div.img-preview-list").append("<div class='img-preview preview' id='"+photos.length+"'><img src='"+e.target.result+"' sizeimg='"+e.total+"'><i class='fa fa-remove remove'></i></div>");
+                  $("div.img-preview-list").append("<div class='img-preview preview' id='"+e.total+"'><img src='"+e.target.result+"' sizeimg='"+e.total+"'><i class='fa fa-remove remove'></i></div>");
                   return;
                }else if (e.target.result === photos[i] && e.total === size[r]) {
                   return;
                }else if (e.target.result !== photos[i] && e.total !== size[i]) {
-                  $("div.img-preview-list").append("<div class='img-preview preview' id='"+photos.length+"'><img src='"+e.target.result+"' sizeimg='"+e.total+"'><i class='fa fa-remove remove'></i></div>");
+                  $("div.img-preview-list").append("<div class='img-preview preview' id='"+e.total+"'><img src='"+e.target.result+"' sizeimg='"+e.total+"'><i class='fa fa-remove remove'></i></div>");
                   return;
                }
             }
          }
+         document.getElementById("photos").value="";
       }
       reader.readAsDataURL(image.files[x]);
    }
@@ -42,13 +50,16 @@ $("div.img-preview-list").on("mouseover", "div.img-preview", function(){
 $("div.img-preview-list").on("click", ".remove", function(){
    var idimg = $(this).parent().attr("id");
    var idarray = $(this).parent().find("img").attr("sizeimg");
-   console.log("photo: "+photos.length+"-sixe: "+size.length);
    var index = size.indexOf(Number(idarray));
-   console.log(index);
    if (index > -1) {
-      photos.splice(index, 1);
-      size.splice(index, 1);
-      $("#"+idimg).remove();
-      console.log("photo: "+photos.length+"-sixe: "+size.length);
+     for (var i = 0; i < arrayInputFiles.length; i++) {
+       if (arrayInputFiles[i].size === size[index]) {
+         arrayInputFiles.splice(index, 1);
+       }
+     }
+     photos.splice(index, 1);
+     size.splice(index, 1);
+     $("#"+idimg).remove();
+     document.getElementById("photos").value="";
    }
 });
